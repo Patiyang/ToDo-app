adbc4757264da853d6224545b7b7a800f3884613
Patiyang <patiyang6@gmail.com>
2020-01-21 10:51:39 +0300

    deleting

--- backend/migrations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#running the migrations
-
-python "/home/patto/PROJECTS/Flutter/todo_app/backend/migrate.py"db init
-python "/home/patto/PROJECTS/Flutter/todo_app/backend/migrate.py"db migrate
-python "/home/patto/PROJECTS/Flutter/todo_app/backend/migrate.py"db upgrade
--- backend/venv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Let's call this script venv.sh
-source "/home/patto/PROJECTS/Flutter/todo_app/backend/env/bin/activate" 
-#&& python "/home/patto/PROJECTS/Flutter/todo_app/backend/run.py"
